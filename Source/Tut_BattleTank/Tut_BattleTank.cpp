74a595b6dc0323bd3e140b0f69809a096a2405f2
Mohit Agrawal <mohit.agrawal2994@gmail.com>
2018-01-20 00:50:06 +0530

    Initial commit

---
+++ Source/Tut_BattleTank/Tut_BattleTank.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tut_BattleTank.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tut_BattleTank, "Tut_BattleTank" );
---
+++ Source/Tut_BattleTank/Tut_BattleTankGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Tut_BattleTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUT_BATTLETANK_API ATut_BattleTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
